53b34edbc9142c90343c9ff5150f6fabc1f67897
Recep Aslantas <info@recp.me>
2022-04-07 00:04:32 +0300

    add deps

---
+++ src/io/ppm.c
@@ -0,0 +1,8 @@
+//
+//  ppm.c
+//  rays
+//
+//  Created by Recep Aslantas on 4/6/22.
+//
+
+#include "ppm.h"
---
+++ src/io/ppm.h
@@ -0,0 +1,13 @@
+//
+//  ppm.h
+//  rays
+//
+//  Created by Recep Aslantas on 4/6/22.
+//
+
+#ifndef ppm_h
+#define ppm_h
+
+#include <stdio.h>
+
+#endif /* ppm_h */
