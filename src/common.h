e63d7bd6c9d4dc1027d185ee3881c6b8cac80190
Recep Aslantas <info@recp.me>
2022-04-09 16:24:39 +0300

    Update common.h

--- src/common.h
+++ src/common.h
@@ -18,9 +18,31 @@
 #define src_common_h
 
 #include "../include/rays/common.h"
+#include "../include/rays/assets.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 #include <cglm/cglm.h>
+
+#define RAYS_ARRAY_LEN(ARR) (sizeof(ARR) / sizeof(ARR[0]))
+
+#ifdef __GNUC__
+#  define RAYS_DESTRUCTOR  __attribute__((destructor))
+#  define RAYS_CONSTRUCTOR __attribute__((constructor))
+#else
+#  define RAYS_DESTRUCTOR
+#  define RAYS_CONSTRUCTOR
+#endif
+
+#ifdef RAYS_WINAPI
+/*
+ #  define WIN32_LEAN_AND_MEAN
+ #  include <windows.h>
+ */
+#  define strncasecmp _strnicmp
+#  define strcasecmp  _stricmp
+#endif
+
+#define RAYS__UNUSED(X) (void)X
 
 #endif /* src_common_h */
